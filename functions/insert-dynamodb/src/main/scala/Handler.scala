9e1fa11f7631ca8c7c64b7d4f4a573157920bdb2
takapi327 <t.takapi0327@gmail.com>
2024-11-29 14:40:01 +0900

    Update Insert dynamodb function

--- functions/insert-dynamodb/src/main/scala/Handler.scala
+++ functions/insert-dynamodb/src/main/scala/Handler.scala
@@ -5,22 +5,21 @@ import org.http4s.ember.client.EmberClientBuilder
 import smithy4s.aws.*
 import com.amazonaws.dynamodb.*
 
-object Handler extends IOLambda.Simple[Unit, Unit]:
+object Handler extends IOLambda.Simple[Unit, String]:
 
   override type Init = DynamoDB[IO]
 
   override def init: Resource[IO, Init] =
     for
       httpClient <- EmberClientBuilder.default[IO].build
-      awsEnv <- AwsEnvironment.default(httpClient, AwsRegion.AP_NORTHEAST_1)
-      dynamodb <- AwsClient(DynamoDB.service, awsEnv)
+      dynamodb <- LocalstackClient(httpClient, AwsRegion.AP_NORTHEAST_1, DynamoDB.service)
     yield dynamodb
 
-  override def apply(event: Unit, context: Context[IO], init: Init): IO[Option[Unit]] =
+  override def apply(event: Unit, context: Context[IO], init: Init): IO[Option[String]] =
     init.putItem(
       TableName("Smithy4sSandboxTable"),
       Map(
-        AttributeName("id") -> AttributeValue.n(NumberAttributeValue("1")),
+        AttributeName("id") -> AttributeValue.s(StringAttributeValue("1")),
         AttributeName("name") -> AttributeValue.s(StringAttributeValue("Alice"))
       )
-    ).as(None)
+    ).as(Some("Success"))
